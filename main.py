0385e2ca1fee923cc2fc29f6dd61a260f67e4750
Bernard Oswald <bernard.oswald@free.fr>
2021-12-25 18:57:31 +0100

    Version 0

---
+++ main.py
@@ -0,0 +1,14 @@
+from view.start import Screen
+#from controller.control import Control
+from view.mainMenuWindow import WinMenu
+#from tkinter import *
+#from view import test
+
+def main():
+	#Screen()
+	WinMenu()
+	#Control()
+	
+
+if __name__ == '__main__':
+	main()
