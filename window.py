c30abd673ee4b71723b8f04821775ece14ebc9d8
Bernard Oswald <bernard.oswald@free.fr>
2022-06-26 17:43:04 +0200

    Ajustement fenêtre

--- window.py
+++ window.py
@@ -9,7 +9,7 @@ class Window:
     def init_window(self):
         root = Tk()
         root.title('Gestion Tournoi d\'echecs')
-        root.geometry("1024x860")
+        root.geometry("1280x860")
         root.config(background='#9a031e')
         root.iconbitmap("./img/logo.ico")
         root.option_add('*tearOff', FALSE)  # Supprime le séparateur
