5da2b46188f61bf5b2a3e89f40bb6dc64abfb96d
Bernard Oswald <bernard.oswald@free.fr>
2022-07-25 18:03:18 +0200

    Ajustements MVC.

--- control/playerController.py
+++ control/playerController.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from model.player import Player
-from tinydb import where
+from tinydb import where, Query
 from model.dbInterface import Interface
 import tkinter as tk
 
@@ -11,13 +11,11 @@ class PlayerController:
         self.model_interface = Interface()
         self.start_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    def add_player_tree_frame(self, input_list, frame, tree_frame,
-                              y, tournament_name, add_player_button,
-                              data_fields):
+    def reg_tournament_player(self, tournament_name, input_list, count):
         data = list()
         data.append(tournament_name)
         data_check = True
-        count = len(tree_frame.get_children())
+        # count = len(tree_frame.get_children())
 
         for element in input_list:
             data.append(element.get())
@@ -29,75 +27,7 @@ class PlayerController:
             player = Player(data)
             player.serialize_player()
             player.write_data()
-
-        tree_frame.tag_configure('oddrow', background="white")
-        tree_frame.tag_configure('evenrow', background="lightblue")
-
-        print("count:", count)
-        if count % 2 == 0:
-            tree_frame.insert('', 'end', text='', values=data, tags='evenrow')
-        else:
-            tree_frame.insert('', 'end', text='', values=data, tags='oddrow')
-        count += 1
-
-        print("data:", data)
-
-    def modify_one_record(self, tree_frame, f_name_box, l_name_box, date_box, gender_var,
-                          radiobutton1, radiobutton2, class_spin_box, tournament_name):
-
-        players_table = self.model_interface.set_db_players_env()
-        selected = tree_frame.focus()
-        value = tree_frame.item(selected, 'values')
-        if radiobutton1.invoke == "Homme":
-            gender_var = "Homme"
-        elif radiobutton2.invoke == "Femme":
-            gender_var = "Femme"
-        # Updating treeview
-        tree_frame.item(selected, text="", values=(
-                        tournament_name,
-                        f_name_box.get(),
-                        l_name_box.get(),
-                        date_box.get(),
-                        gender_var.get(),
-                        class_spin_box.get(),
-                        value[6]))
-
-        name = f_name_box.get()
-        players_table.update({'first_name': f_name_box.get()}, where('first_name') == name)
-        players_table.update({'last_name': l_name_box.get()}, where('first_name') == name)
-        players_table.update({'birth_date': date_box.get()}, where('first_name') == name)
-        players_table.update({'gender': gender_var.get()}, where('first_name') == name)
-        players_table.update({'rank': class_spin_box.get()}, where('first_name') == name)
-        players_table.update({'score': 0}, where('first_name') == name)
-
-    def clear_entries(self, f_name_box, l_name_box, date_box, gender_var, radiobutton1, radiobutton2, class_spin_box):
-
-        f_name_box.delete(0, tk.END)
-        l_name_box.delete(0, tk.END)
-        date_box.delete(0, tk.END)
-        gender_var.set(None)
-        class_spin_box.delete(0, tk.END)
-
-    def select_one_record(self, tree_frame,
-                          f_name_box,
-                          l_name_box,
-                          date_box,
-                          gender_var,
-                          radiobutton1,
-                          radiobutton2,
-                          class_spin_box):
-        self.clear_entries(f_name_box, l_name_box, date_box, gender_var, radiobutton1, radiobutton2, class_spin_box)
-        selected = tree_frame.focus()
-        values = tree_frame.item(selected, 'values')
-
-        f_name_box.insert(0, values[1])
-        l_name_box.insert(0, values[2])
-        date_box.insert(0, values[3])
-        if values[4] == "Homme":
-            radiobutton1.invoke()
-        else:
-            radiobutton2.invoke()
-        class_spin_box.insert(0, values[5])
+        return data
 
     def quit_player_window(self):
         self.p_frame.destroy()
@@ -108,15 +38,6 @@ class PlayerController:
 
     def quitPlayerWindow(self):
         self.frame.destroy()
-
-    def delete_one_player_button(self, tree_frame):
-        player_selected = tree_frame.focus()
-        # tournament_selected = n° ligne, value = valeurs colonnes
-        temp = tree_frame.item(player_selected, 'values')
-
-        for element in tree_frame.selection():
-            tree_frame.delete(player_selected)
-            self.delete_player_data(temp[1])  # nom du joueur
 
     def delete_all_players_button(self, tree_frame):
         tournament_name = tree_frame.item('I002', 'values')[0]
